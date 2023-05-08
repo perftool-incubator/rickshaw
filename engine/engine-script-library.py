b98771a1e91262db68cd5cc82c5016806cbbcb79
Greg Dumas <grdumas@users.noreply.github.com>
2023-05-08 11:27:12 -0400

    Revision of do_roadblock()

--- engine/engine-script-library.py
+++ engine/engine-script-library.py
@@ -33,104 +33,124 @@ def abort_error(msg, sync):
     global abort
     abort = 1
 
-def do_roadblock(label, timeout, *args):
-    print(f"do_roadblock() ARGC: {len(args)}")
-    print(f"do_roadblock() ARGS: {args}")
+def do_roadblock(label, timeout, messages=None, wait_for=None, abort=False):
+    """
+    Executes a roadblock with the specified label, timeout, and optional parameters for sending a user message file,
+    waiting for a particular condition, and aborting the roadblock.
 
+    :param label: A label for the roadblock
+    :param timeout: The timeout value for the roadblock
+    :param messages: (Optional) A path to a file containing user messages to send
+    :param wait_for: (Optional) A command to wait for before proceeding with the roadblock
+    :param abort: (Optional) If True, sends an abort message to the roadblock
+    :return: The exit code of the roadblock command
+    """
+
+    # Set the leader for the roadblock to be the "controller"
     leader = "controller"
-    message = ""
-    wait_for = ""
-    wait_for_log = ""
-    do_abort = 0
+    
+    wait_for_log = None
 
-    arg_iter = iter(args)
-    for arg in arg_iter:
-        if arg == "messages":
-            message = next(arg_iter)
-            print(f"Going to send this user message file: {message}")
-        elif arg == "wait-for":
-            wait_for = next(arg_iter)
-            wait_for_log = tempfile.mkstemp()[1]
-            print(f"Going to run this wait-for command: {wait_for}")
-            print(f"Going to log wait-for to this file: {wait_for_log}")
-        elif arg == "abort":
-            do_abort = 1
-            print("Going to send an abort")
-        else:
-            exit_error(f"[ERROR]do_roadblock() encountered invalid optional parameter(s) {args}")
+    # If messages is not None, print a message indicating that the user message file is going to be sent
+    if messages is not None:
+        print("Going to send this user message file:", messages)
 
-    if not message:
-        print("Not going to send a user message file")
-    if not wait_for:
-        print("Not going to use wait-for")
-    if not label:
-        exit_error("[ERROR]do_roadblock() label not provided")
-    if not timeout:
-        exit_error("[ERROR]do_roadblock() timeout not provided")
+    # If wait_for is not None, print a message indicating that the wait-for command is going to be run
+    # and set wait_for_log to a temporary file path
+    if wait_for is not None:
+        print("Going to run this wait-for command:", wait_for)
+        wait_for_log = tempfile.mktemp()
 
-    msgs_log_file = f"{roadblock_msgs_dir}/{label}.json"
-    cmd = []
+    # Raise a ValueError if the label or timeout are not provided
+    if label is None:
+        raise ValueError("[ERROR]do_roadblock() label not provided")
+
+    if timeout is None:
+        raise ValueError("[ERROR]do_roadblock() timeout not provided")
+
+    # Set the path for the user messages log file
+    msgs_log_file = os.path.join(roadblock_msgs_dir, f"{label}.json")
+
+    # Initialize the command string and set the role to "follower"
+    cmd = ""
     role = "follower"
 
-    # Replace rickshaw_host with the appropriate hostname or IP address
-    rickshaw_host = "localhost"
+    # Use subprocess to run a "ping" command to the rickshaw host
     subprocess.run(["ping", "-w", "10", "-c", "4", rickshaw_host])
 
-    cmd += [roadblock_bin, f"--role={role}", f"--redis-server={rickshaw_host}"]
-    cmd += [f"--leader-id={leader}", f"--timeout={timeout}", f"--redis-password={roadblock_passwd}"]
-    cmd += [f"--follower-id={cs_label}", f"--message-log={msgs_log_file}"]
+    # Append the necessary arguments to the cmd string
+    cmd += f" {roadblock_bin} --role={role} --redis-server={rickshaw_host}"
+    cmd += f" --leader-id={leader} --timeout={timeout} --redis-password={roadblock_passwd}"
+    cmd += f" --follower-id={cs_label} --message-log={msgs_log_file}"
 
-    if message:
-        cmd += ["--user-message", message]
-    if wait_for:
-        cmd += ["--wait-for", wait_for]
-        cmd += ["--wait-for-log", wait_for_log]
-    if do_abort:
-        cmd += ["--abort"]
+    # If messages is not None, append the user message argument to the cmd string
+    if messages is not None:
+        cmd += f" --user-message {messages}"
 
+    # If wait_for is not None, append the wait-for argument and wait_for_log argument to the cmd string
+    if wait_for is not None:
+        cmd += f" --wait-for \"{wait_for}\""
+        cmd += f" --wait-for-log {wait_for_log}"
+
+    # If abort is True, append the abort argument to the cmd string
+    if abort:
+        cmd += " --abort"
+
+    # Set the uuid to be the concatenation of the roadblock_id and the label
     uuid = f"{roadblock_id}:{label}"
+
+    # Print info about the roadblock
     print("\n\n")
-    print(f"Starting roadblock [`date`]")
+    print(f"Starting roadblock [{datetime.datetime.now()}]")
     print(f"server: {rickshaw_host}")
     print(f"role: {role}")
     print(f"uuid (without attempt ID embedded): {uuid}")
     print(f"timeout: {timeout}")
 
+    # Initialize the number of attempts and the exit code
     attempts = 0
     rc = 99
+
+    # Loop until either the maximum number of attempts is reached or the roadblock is successful or aborted
     while attempts < max_rb_attempts and rc != rb_exit_success and rc != rb_exit_abort:
         attempts += 1
         print(f"attempt number: {attempts}")
         print(f"uuid: {attempts}:{uuid}")
-        rb_cmd = cmd + [f"--uuid={attempts}:{uuid}"]
-        print(f"going to run this roadblock command: {' '.join(rb_cmd)}")
+        rb_cmd = f"{cmd} --uuid={attempts}:{uuid}"
+        print("going to run this roadblock command:", rb_cmd)
         print("roadblock output BEGIN")
-        result = subprocess.run(rb_cmd)
-        rc = result.returncode
+
+        # Use subprocess to run the roadblock command and capture the exit code
+        # Set shell=True to interpret the command as a string instead of a list
+        rc = subprocess.run(rb_cmd, shell=True).returncode
         print("roadblock output END")
         print(f"roadblock exit code: {rc}")
 
+        # If the user messages log file exists, print its contents
         if os.path.isfile(msgs_log_file):
             print("# start messages from roadblock ################################################")
-            with open(msgs_log_file, "r") as f:
-                print(f.read())
+            with open(msgs_log_file, "r") as msgs_file:
+                print(msgs_file.read())
             print("# end messages from roadblock ##################################################")
         else:
             print("# no messages from roadblock ###################################################")
 
-        if wait_for_log and os.path.isfile(wait_for_log):
+        # If the wait-for log file exists, print its contents and delete the file
+        if wait_for_log is not None and os.path.isfile(wait_for_log):
             print("# start wait-for log ###########################################################")
-            with open(wait_for_log, "r") as f:
-                print(f.read())
+            with open(wait_for_log, "r") as wait_for_file:
+                print(wait_for_file.read())
             print("# end wait-for log #############################################################")
             os.remove(wait_for_log)
         else:
-            print("# no wait-for log ##############################################################")
+            print("# no wait for log ##############################################################")
 
+    # Print info about the completed roadblock
     print(f"total attempts: {attempts}")
-    print("Completed roadblock [`date`]")
+    print(f"Completed roadblock [{datetime.datetime.now()}]")
     print("\n\n")
 
+    # Return the exit code of the roadblock command
     print(f"returning {rc}")
     return rc
 
